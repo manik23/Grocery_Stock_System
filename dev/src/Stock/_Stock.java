7733c378e342d2bd3bd41b720588f3c443e65e0b
manik23 <manik.x.mahajan@gmail.com>
2021-08-04 13:54:08 +0530

    added consumption

--- dev/src/Stock/_Stock.java
+++ dev/src/Stock/_Stock.java
@@ -1,5 +1,7 @@
 package Stock;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import product.*;
@@ -18,13 +20,35 @@ public class _Stock {
             hash.put(name , new _product(name, quantity, price, profit));
         }
     }
+
+    public double consume_stock(String name, double quantity){
+        if(hash.containsKey(name) && hash.get(name).quantity>0){
+
+            if(hash.get(name).quantity < quantity){
+                double price = hash.get(name).quantity * hash.get(name).price * (1+(hash.get(name).profit/100));
+                hash.get(name).quantity = 0;
+                return price;
+            }
+            else{
+                double price = quantity * hash.get(name).price * (1+(hash.get(name).profit/100));
+                hash.get(name).quantity -= quantity;
+                return price;
+            }
+        }
+        else{
+            System.out.println("Stock not Available");
+            return 0;
+        }
+    }
+
+
     public void _display()
     {
         for(Map.Entry<String,_product> x : hash.entrySet())
         {
             System.out.println(x.getValue());
         }    
-        
+       
     }
 
 
@@ -32,11 +56,15 @@ public class _Stock {
     {
         _Stock obj= new _Stock();
         obj.add_stock("atta",10.0,50.0,20.0);
+        obj.add_stock("rice",0,100.0,15.0);
+        obj.add_stock("corn",10.0,30.0,5.0);
+        obj.add_stock("haldi",10.0,20.0,10.0);
         obj._display();
+        double price = obj.consume_stock("rice",10);
+        System.out.println(price);
 
 
     }
     
 }
 
-// Name, quantity,price
